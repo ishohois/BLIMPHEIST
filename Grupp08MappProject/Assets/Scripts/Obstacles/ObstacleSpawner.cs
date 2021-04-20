91e19eabb516cb308e4e11da556cf91382176854
HannaFrida <expresen.gameing@gmail.com>
2021-04-20 16:49:07 +0200

    Fixade till spawner
    Kan nu spawna flera olika obstacles och på ett mer effective sätt

--- Grupp08MappProject/Assets/Scripts/Obstacles/ObstacleSpawner.cs
+++ Grupp08MappProject/Assets/Scripts/Obstacles/ObstacleSpawner.cs
@@ -4,45 +4,32 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    public int obstaclePoolSize = 5;
-    public GameObject obstaclePrefab;
     public float spawnRate = 4f;
     public float planetMin = -1f;
     public float planetMax = 3.5f;
 
-    private GameObject[] obstacles;
-    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    public GameObject[] obstacles;
     private float timeSinceLatSpawned;
     private float spawnXPosition = 10f;
-    private int currentPlanet = 0;
+    private GameObject currentObstacle;
+    private int index;
 
     public AudioClip obstacleSpawn;
-    // Start is called before the first frame update
-    void Start()
-    {
-        obstacles = new GameObject[obstaclePoolSize];
-        for (int i = 0; i < obstaclePoolSize; i++)
-        {
-            obstacles[i] = (GameObject)Instantiate(obstaclePrefab, objectPoolPosition, Quaternion.identity);
-        }
-    }
 
     // Update is called once per frame
     void Update()
     {
         timeSinceLatSpawned += Time.deltaTime;
+        index = Random.Range(0, obstacles.Length);
+        currentObstacle = obstacles[index];
 
         if (GameController.instance.gameOver == false && timeSinceLatSpawned >= spawnRate)
         {
             timeSinceLatSpawned = 0;
             float spawnYPosition = Random.Range(planetMin, planetMax);
-            obstacles[currentPlanet].transform.position = new Vector2(spawnXPosition, spawnYPosition);
-            currentPlanet++;
+            Vector2 spawnPosition = new Vector2(spawnXPosition, spawnYPosition);
+            Instantiate(currentObstacle, spawnPosition, Quaternion.identity);
             GameController.instance.PlaySound(obstacleSpawn);
-            if (currentPlanet >= obstaclePoolSize)
-            {
-                currentPlanet = 0;
-            }
         }
     }
 }
