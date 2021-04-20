0dd19c72be14977cba9a0ae32e146e73fbfe22a2
ishohois <47395805+ishohois@users.noreply.github.com>
2021-04-20 15:23:22 +0200

    samplescene deleted

--- Grupp08MappProject/Assets/Scripts/Health_Damage_Pickups/Pickup.cs
+++ Grupp08MappProject/Assets/Scripts/Health_Damage_Pickups/Pickup.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public class Pickup : MonoBehaviour, IPickable
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void pickup()
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
