3a14615217559b4cfb762d66d9227e8c1c8ae571
ishohois <47395805+ishohois@users.noreply.github.com>
2021-04-25 18:02:36 +0200

    ObjectPool

---
+++ Grupp08MappProject/Assets/Scripts/ObjectPooling/PoolManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+    public static PoolManager Instance;
+
+    #region
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        foreach (Pool pool in pools)
+        {
+            if (!poolDictionary.ContainsKey(pool.tag))
+            {
+                poolDictionary.Add(pool.tag, new Queue<GameObject>());
+            }
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                poolDictionary[pool.tag].Enqueue(obj);
+            }
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+
+
+    public class PoolObject
+    {
+        GameObject gameObject;
+
+    }
+
+}
+
