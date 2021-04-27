e640f7fa9d84b4c4aa144d0a296694a9ecc3f33b
HannaFrida <expresen.gameing@gmail.com>
2021-04-27 11:24:17 +0200

    Revert "Fixed sound updating"
    This reverts commit 15c3ac3966ec452c5352b87cbeb912e44f9c28fb.

--- Grupp08MappProject/Assets/Scripts/Menu/ShowValueScript.cs
+++ Grupp08MappProject/Assets/Scripts/Menu/ShowValueScript.cs
@@ -6,31 +6,14 @@ using UnityEngine.UI;
 public class ShowValueScript : MonoBehaviour
 {
     private Text percentageText;
-    [SerializeField] Slider slider;
 
     private void Start()
     {
         percentageText = GetComponent<Text>();
-        //percentageText.text = Mathf.RoundToInt(int.Parse(percentageText.text) * 100) + "%";
     }
 
     public void textUpdate(float value)
     {
-        if(value!=null && percentageText != null)
-        {
-            percentageText.text = Mathf.RoundToInt(value * 100) + "%";
-        }
-        else
-        {
-            Debug.Log(value + "  null");
-
-        }
-    }
-
-    public void OpenMenu()
-    {
-        Debug.Log(slider.value);
-        textUpdate(slider.value);
-
+        percentageText.text = Mathf.RoundToInt(value * 100) + "%";
     }
 }
