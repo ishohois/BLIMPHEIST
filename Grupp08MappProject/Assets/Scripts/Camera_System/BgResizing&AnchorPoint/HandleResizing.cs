abdf4383c138a937c11a3845609f1aacdf80dbfa
ishohois <47395805+ishohois@users.noreply.github.com>
2021-05-07 12:31:55 +0200

    BirdFlyAnimationUpdate

--- Grupp08MappProject/Assets/Scripts/Camera_System/BgResizing&AnchorPoint/HandleResizing.cs
+++ Grupp08MappProject/Assets/Scripts/Camera_System/BgResizing&AnchorPoint/HandleResizing.cs
@@ -7,11 +7,18 @@ public class HandleResizing : MonoBehaviour
     private Camera main;
 
     public SpriteRenderer sr;
+    //public Collider2D collider;
 
-    // Start is called before the first frame update
-    void Start()
+
+    //public bool usingSRBounds;
+
+    private void Awake()
     {
         main = Camera.main;
+    }
+
+    void Start()
+    {
 
         ResizingBackground();
     }
@@ -21,9 +28,16 @@ public class HandleResizing : MonoBehaviour
         var width = sr.bounds.size.x;
         var height = sr.bounds.size.y;
 
+        //var widthCollider = collider.bounds.size.x;
+        //var heightCollider = collider.bounds.size.y;
+
         var worldScreenHeight = main.orthographicSize * 2f;
 
         var worldScreenWidth = worldScreenHeight * main.aspect;
+
+        //transform.localScale = usingSRBounds ?  
+        //    new Vector3(worldScreenWidth / width, worldScreenHeight / height, transform.localScale.z) : 
+        //    new Vector3(worldScreenWidth / widthCollider, worldScreenHeight / heightCollider, transform.localScale.z);
 
         transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, transform.localScale.z);
     }
