2a172873ed729b70a7a6e98e91d123f531afc991
Milizana <kaplia.maria@gmail.com>
2021-05-11 22:12:24 +0200

    Parallax
    Hastigheten behöver tweakas, ordningen  på lagren kan se lite skev ut atm

---
+++ Grupp08MappProject/Assets/Scripts/Camera_System/Parallax/BG_Scroller_Control.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_Scroller_Control : MonoBehaviour {
+
+    //ZTest Always
+    public float scrollSpeed;
+    public Renderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0f);
+
+    }
+}
