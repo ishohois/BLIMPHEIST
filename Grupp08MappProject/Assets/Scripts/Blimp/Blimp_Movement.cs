3dcbcf9075c67459a0d62f15c9d05523dc5b7b71
HannaFrida <expresen.gameing@gmail.com>
2021-05-17 19:21:37 +0200

    Ny burst SFX
    Kortare burst ljudeffekt

--- Grupp08MappProject/Assets/Scripts/Blimp/Blimp_Movement.cs
+++ Grupp08MappProject/Assets/Scripts/Blimp/Blimp_Movement.cs
@@ -160,7 +160,7 @@ public class Blimp_Movement : MonoBehaviour
     {
         if (isBurstAvailable == true)
         {
-            burstSound.pitch = Random.Range(0.9f, 1.0f);
+            burstSound.pitch = Random.Range(0.9f, 1.0f); //1.0f, 1.1f Låter också ok
             burstSound.Play(); //Spela ljud när man använder burst
 
             burstUsed = true;
@@ -226,7 +226,7 @@ public class Blimp_Movement : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.D) == true)
             {
-                burstSound.pitch = Random.Range(0.9f, 1.0f);
+                burstSound.pitch = Random.Range(0.9f, 1.0f); //1.0f, 1.1f Låter också ok
                 burstSound.Play(); //Spela ljud när man använder burst
 
                 burstUsed = true;
