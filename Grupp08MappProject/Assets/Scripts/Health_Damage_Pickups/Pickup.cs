2e06007b53076d81244b33e23f40c8581ac38f5c
Mikita28 <nikitakigan@gmail.com>
2021-05-19 16:20:49 +0200

    Justeringar med ljudeffekter för pickups

--- Grupp08MappProject/Assets/Scripts/Health_Damage_Pickups/Pickup.cs
+++ Grupp08MappProject/Assets/Scripts/Health_Damage_Pickups/Pickup.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public abstract class Pickup : MonoBehaviour, IPickable
 {
 
-    [SerializeField] private float deactivationTime = 1f;
+    [SerializeField] protected float deactivationTime = 1f;
 
     public abstract void GiveEffect(PlayerState player);
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // logic for deactivating particle effects yada yada
-        gameObject.SetActive(false);
-    }
 
 }
